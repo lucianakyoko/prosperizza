27458750f0dc5c79ec04b9a7ad91a4079e06bb4d
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2024-03-19 18:48:07 -0300

    style(app/Screens/HomeScreen): create PhraseSection component

---
+++ src/app/Screens/HomeScreen/PhraseSection/index.jsx
@@ -0,0 +1,11 @@
+export const PhraseSection = () => {
+  return (
+    <section className="py-6 px-4 sm:px-0 font-medium border-t border-brown-100 border-dashed flex items-center gap-8 lg:gap-9 lg:justify-between">
+      <div className="hidden sm:block sm:w-[201px] lg:w-[316px] sm:h-[298px] lg:h-[468px] bg-cheese bg-contain bg-no-repeat bg-left"></div>
+      <div className="text-gray-950 text-2xl  sm:text-3xl lg:text-4xl sm:w-[500px] lg:w-[749px] sm: pr-5">
+        <p>Na Prosperizza, cada mordida é um commit para a felicidade!</p>
+        <p>Junte-se a nós e saboreie o código do sabor!</p>
+      </div>
+    </section>
+  );
+};
