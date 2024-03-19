def370997efce619efb8e10ae89437f8e2cd5a75
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2024-03-19 18:48:36 -0300

    style(app/Screens/HomeScreen): create PromotionSection component

---
+++ src/app/Screens/HomeScreen/PromotionSection/PromotionItem/index.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { ModalContext } from "@/app/context/ModalContext";
+import { Title } from "@/components/Title";
+import { ShoppingCartSimple } from "@phosphor-icons/react/dist/ssr";
+
+export const PromotionItem = () => {
+  const {handleModal} = useContext(ModalContext);
+  return (
+    <li className="bg-red-200 w-[328px] sm:w-[483px] rounded-2xl flex flex-col sm:flex-row gap-4 items-center py-6 sm:px-6">
+      <div className="relative w-[263px] h-[263px] sm:h-[234px] lg:h-[216px] rounded-2xl cursor-pointer">
+        <img src="/test-img.png"  className="w-full"/>
+        <div 
+            className="absolute inset-0 hover:bg-black-900 rounded-2xl flex justify-center items-center opacity-0 hover:opacity-100"
+            onClick={handleModal}
+          >
+            <ShoppingCartSimple weight="fill" className="text-yellow-100 text-4xl" />
+          </div>
+      </div>
+
+      <div className="text-gray-100 flex flex-col gap-4 items-center sm:items-start">
+        <Title type='light'>Título Promoção</Title>
+        <div className="flex items-center gap-2 sm:items-start">
+          <span className="text-xl">R$</span>
+          <span className="text-4xl font-bold">39,90</span>
+        </div>
+        <p>todas as sextas-feiras</p>
+      </div>
+    </li>
+  );
+}
---
+++ src/app/Screens/HomeScreen/PromotionSection/index.jsx
@@ -0,0 +1,23 @@
+'use client';
+import { useContext } from "react";
+import { ModalContext } from "@/app/context/ModalContext";
+
+import { Title } from "@/components/Title";
+import { PromotionItem } from "./PromotionItem";
+import { PizzaDetails } from "@/components/PizzaDetails";
+
+export const PromotionSection = () => {
+  const {openModal} = useContext(ModalContext);
+
+  return (
+    <section className="flex flex-col gap-5 lg:gap-6">
+      <Title type='dark'>Promoções Que Debugam A Fome</Title>
+      <ul className="flex flex-col gap-4 lg:gap-6 items-center sm:items-start lg:flex-row">
+        <PromotionItem />
+        <PromotionItem />
+      </ul>
+
+      { openModal && <PizzaDetails />}
+    </section>
+  );
+}
