926c5b133cdc0cd8f6016e650f234eb268e215df
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2024-03-19 18:47:36 -0300

    style(app/Screens/HomeScreen): create FavoriteSection component

---
+++ src/app/Screens/HomeScreen/FavoritesSection/index.jsx
@@ -0,0 +1,33 @@
+'use client';
+import NextLink from "next/link";
+import { useContext } from "react";
+import { ModalContext } from "@/app/context/ModalContext";
+import { PizzaDetails } from "@/components/PizzaDetails";
+import { PizzaItemCard } from "@/components/PizzaItemCard";
+import { Title } from "@/components/Title";
+
+export const FavoritesSection = () => {
+  const {openModal} = useContext(ModalContext);
+
+  return(
+    <section className="mt-7 sm:mt-9 lg:mt-12 flex flex-col gap-5 lg:gap-6">
+      <div className="flex items-center justify-between">
+        <Title type='dark'>As Preferidas Dos Coders</Title>
+        <NextLink href='/cardapio'>
+          <span className="text-red-100 text-sm underline">ver todos</span>
+        </NextLink>
+      </div>
+
+      <ul className="flex flex-wrap gap-4 sm:gap-6 justify-start xl:justify-between">
+        <PizzaItemCard />
+        <PizzaItemCard />
+        <PizzaItemCard />
+        <PizzaItemCard />
+        <PizzaItemCard />
+        <PizzaItemCard />
+      </ul>
+      { openModal && <PizzaDetails /> }
+      
+    </section>
+  );
+}
